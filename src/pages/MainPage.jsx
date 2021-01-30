cddc8cfac14c6f5211e50d91698e95202981eb36
Nika Yakubova <lilit666an@gmail.com>
2021-01-30 20:00:00 +0300

    Add styles for titles

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,7 +4,9 @@ import PropTypes from 'prop-types'
 const MainPage = props => {
     return (
         <div>
-            <h1>Main Page</h1>
+            <h1 className='h1'>PROJECT<br/><span className='h1__bold'>NURTOWN</span></h1>
+            <h2 className='h2'>О компании предложение</h2>
+            <h3 className="h3">Общеобразовательная школа</h3>
         </div>
     )
 }
