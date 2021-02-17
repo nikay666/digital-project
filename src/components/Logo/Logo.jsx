199f2afc83aadf70e37447f9c0d27c5477531691
Nika Yakubova <lilit666an@gmail.com>
2021-02-17 20:00:00 +0300

    Styles: add styles for logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import darkLogoSvg from '../../assets/img/darkLogo.svg'
 import lightLogoSvg from '../../assets/img/lightLogo.svg'
 import { Link } from 'react-router-dom'
+import './Logo.scss'
 
 const Logo = ({type='dark'}) => {
     const logo = type === 'dark' ? darkLogoSvg  : lightLogoSvg
